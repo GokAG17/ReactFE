149562ea5d9b7577a34b7d11ed754913dc2f33ef
Gokulraj <gokulraj984298@gmail.com>
2023-10-30 07:47:24 +0530

    api https  1 commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Disable SSL certificate validation globally
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
